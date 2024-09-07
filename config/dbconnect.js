cd09f4093418bac8b3818a302e4af83dc557fa81
Nazim Saifi <saifinazim2311@gmail.com>
2024-09-07 19:39:59 +0530

    Added a log

--- config/dbconnect.js
+++ config/dbconnect.js
@@ -2,7 +2,8 @@ import mongoose from 'mongoose'
 const dbconnect=async()=>{
 mongoose.connect(process.env.DATABASE_URL).then(()=>{
     console.log("db connected successfully")
-}).catch(e=>{
+}).catch((error)=>{
+    console.log(error)
     console.log("issue while connecting with db")
 })
 }
