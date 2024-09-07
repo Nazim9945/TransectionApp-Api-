11ef87ebd9e45d418d16a85d58ef463d12499f37
Nazim Saifi <saifinazim2311@gmail.com>
2024-09-07 17:28:36 +0530

    remove unwanted console

--- config/dbconnect.js
+++ config/dbconnect.js
@@ -3,7 +3,7 @@ const dbconnect=async()=>{
 mongoose.connect(process.env.DATABASE_URL).then(()=>{
     console.log("db connected successfully")
 }).catch(e=>{
-    console("issue while connecting with db")
+    console.log("issue while connecting with db")
 })
 }
 export default dbconnect
